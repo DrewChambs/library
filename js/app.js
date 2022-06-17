c18ed26e6b29e6dca47e366384ae930947f507d1
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-06-17 13:55:03 -0400

    Re-factored all code. Event fix after browser painted

--- js/app.js
+++ js/app.js
@@ -2,39 +2,110 @@
 ///   10 de abril, 2022    /////
 ////////////////////////////////
 
-// Display div
-const showBooks = document.querySelector(".display-books");
+// Display
+const tableContainer = document.querySelector(".table-container");
 
-let myLibrary = [];
-
+// Book object
 function Book(title, author, pages, read) {
   (this.title = title),
     (this.author = author),
     (this.pages = pages),
-    (this.read = false);
-  this.info = function () {
-    console.log(
-      `The book, ${this.title}, by ${this.author} has ${
+    (this.read = read),
+    (this.info = function () {
+      return `<p>"${this.title}",  by ${this.author} has ${
         this.pages
-      } pages and I've ${read ? "read this book" : "not read this book."} `
-    );
-  };
+      } pages and I've ${
+        read ? "read this book" : "not read this book."
+      }</p><br>`;
+    });
 }
 
+// Array to hold library books
+let myLibrary = [];
+
+// Function to add to Library
 function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
-
-  let displayBook = myLibrary.map(item => {
-    return `<p>The book "${item.title}." is written by ${item.author}.</p>
-    <p>This book has ${item.pages} pages and I have not read it</p>`;
-  });
-  console.log(displayBook);
-  showBooks.innerHTML = displayBook;
+  return myLibrary;
 }
-const mybook = new Book("When the wind blows", "Ben Jamin", 235, true);
-const mybook2 = new Book("Find a Way", "Gooly Gim", 1235, false);
 
-// console.log(addBookToLibrary(mybook));
+const mybook = new Book("When the wind blows", "Ben Jamin", 235, true);
+
+const mybook2 = new Book(
+  "Call All the People in Town",
+  "James Rooland-Kent",
+  433,
+  true
+);
+const mybook3 = new Book(
+  "Silly Games for the Masses",
+  "Jonathon Miles",
+  245,
+  false
+);
+const mybook4 = new Book("Find a Way", "Gooly Gim", 1235, true);
 
 addBookToLibrary(mybook);
 addBookToLibrary(mybook2);
+addBookToLibrary(mybook3);
+addBookToLibrary(mybook4);
+console.log(myLibrary);
+
+// FUNCTIONS //
+const pageLibraryDisplay = book_libray => {
+  let newBookLibrary = book_libray.map(item => {
+    return `<table>
+            <tr>
+              <th class="book-title-heading">Book Title</th>
+              <td class="book-title">
+                ${item.title}
+              </td>
+            </tr>
+            <tr>
+              <th>Author</th>
+              <td>${item.author}</td>
+            </tr>
+            <tr>
+              <th>Pages</th>
+              <td>${item.pages}</td>
+            </tr>
+            <tr>
+              <th>Completed</th>
+              <td class="read-book">${item.read ? "Yes" : "No"}</td>
+            </tr>
+            <tr>
+              <th class="change-details">Change Details:</th>
+              <td>
+                <div class="btn-holder">
+                  <button class="read-btn">Read Book</button>
+                  <button class="delete-btn">Delete Book</button>
+                </div>
+              </td>
+            </tr>
+          </table>`;
+  });
+  tableContainer.innerHTML = newBookLibrary;
+};
+
+// Display on Page
+pageLibraryDisplay(myLibrary);
+
+// Function to change book "read" status
+function readStatus() {}
+
+// Buttons SELECT
+const addBtn = document.querySelector(".add-btn");
+const deleteBtn = document.querySelector(".delete-btn");
+const readBtn = document.querySelector(".read-btn");
+
+// Events
+deleteBtn.addEventListener("click", () => {
+  console.log("Delete button!");
+});
+addBtn.addEventListener("click", () => {
+  console.log("Add Suppen yo!");
+});
+readBtn.addEventListener("click", () => {
+  console.log("Read this book!");
+  document.querySelector(".read-book").textContent = "Yes";
+});
