8af2efeb15b173e27b5ca8251962a3add2f914e9
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-08-26 17:00:01 -0400

    Change book status read Yes/No

--- js/app.js
+++ js/app.js
@@ -1,203 +1,237 @@
 ////////////////////////////////
 ///   10 de junio, 2022    /////
 ////////////////////////////////
-
 // 2 de agosto, 2022
 // 6 de agosto, 2022 update
-
+// Sábado, 20 de agosto, 2022 //
 // Book Object
 function Book(title, author, pages, read) {
   (this.title = title),
     (this.author = author),
     (this.pages = pages),
-    (this.read = read),
-    (this.info = function () {
-      return `<p>"${this.title}",  by ${this.author} has ${
-        this.pages
-      } pages and I've ${
-        read ? "read this book" : "not read this book."
-      }</p><br>`;
-    });
+    (this.read = read);
+  this.id = Math.round(Math.random() * 100000);
+  this.info = function () {
+    return `<p>"${this.title}",  by ${this.author} has ${
+      this.pages
+    } pages and I've ${
+      read ? "read this book" : "not read this book."
+    }</p><br>`;
+  };
 }
+// New Book Array
+let sepLibrary = [];
 
-// Book Array
-let myLibrary = [];
-// Counter to add id to books
-let bookCounter = 0;
+// Form
+// const mainForm = document.getElementById("main-form");
+const libraryForm = document.getElementById("forms");
+//
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("book-read");
 
-// Display div
+// Table Container
 const tableContainer = document.querySelector(".table-container");
-
-// Add Button
-const btnAdd = document.querySelector(".btn-add");
+// Table Headgings ---
+const tableHeaders = ["Title", "Author", "Pages", "Read", "Read/Edit/Delete"];
+// Buttons
+const submitBtn = document.querySelector(".btn-submit");
+const addBtn = document.querySelector(".btn-add");
 const clearBtn = document.querySelector(".btn-clear");
-const submitBtn = document.querySelector(".submit-btn");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".fa-xmark");
 
-// Table Headgings ---
-const titleNames = ["Title", "Author", "Pages", "Read", "Edit"];
-
 // Create table
 const table = document.createElement("table");
+// Call form
+// mainForm.addEventListener("submit", addBookToLibrary);
+libraryForm.addEventListener("submit", addBookToLibrary);
+// Load table headings
+loadHeadings(tableHeaders);
 
-// Select form
-const form = document.querySelector(".book-information-form");
+// *** Retrieve Library from localStorage **** //
+sepLibrary = JSON.parse(localStorage.getItem("sepLibrary"));
 
-// Book Select
-const bookTitle = document.querySelector("#book-title");
-const bookAuthor = document.querySelector("#author");
-const bookPages = document.querySelector("#pages");
-const bookCompleted = document.querySelector("#completed");
-
-form.addEventListener("submit", addBookToLibrary);
-
-// load headings
-loadHeadings(titleNames);
-
-// -------------------------- //
 // FUNCTIONS
 function addBookToLibrary(e) {
   e.preventDefault();
-  if (
-    bookTitle.value === "" ||
-    bookAuthor.value === "" ||
-    bookPages.value === "" ||
-    bookCompleted.value === ""
-  ) {
-    alert("You must complete all fields, or close out entry form");
-  } else {
-    addToLocalStorage();
-    location.reload();
-  }
-}
+  //
+  let newBook = new Book(title.value, author.value, pages.value, read.value);
 
-// Load table headings
-function loadHeadings(titleArray) {
+  // Check for empty fields
+  if (
+    title.value === "" ||
+    author.value === "" ||
+    pages.value === "" ||
+    read.value === ""
+  ) {
+    title.focus();
+    return;
+  }
+  // Add new book & load local storage
+  addItemsInLocalStorage(newBook);
+  resetAllForm();
+}
+//
+function loadHeadings(tableHeaders) {
   const tr = document.createElement("tr");
-  titleArray.forEach(item => {
+  tableHeaders.forEach(item => {
     const th = document.createElement("th");
-    th.classList.add("center-text");
+    th.classList.add("table-header");
     th.textContent = `${item}`;
     tr.appendChild(th);
     table.appendChild(tr);
     tableContainer.appendChild(table);
   });
 }
-
-// *** Retrieve Library from localStorage **** //
-myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-
-function displayOnPage(oldLibrary) {
-  oldLibrary.forEach(item => {
+//
+function createBookData(libraryArray) {
+  libraryArray.forEach((item, index) => {
     const tr = document.createElement("tr");
-    tr.id = bookCounter++;
-    tr.innerHTML = `
-             <td>${item.title}</td>
-             <td>${item.author}</td>
-             <td class="center-text">${item.pages}</td>
-             <td class="center-text">${item.read}</td>`;
+    tr.id = index;
+    tr.classList.add("table-data");
+    tr.innerHTML = `<td class="display-title">${item.title}</td>
+              <td class="display-author">${item.author}</td>
+              <td class="text-center display-pages">${item.pages}</td>
+              <td class="text-center read-status display-read">${item.read}</td>
+              `;
     table.appendChild(tr);
 
     // Buttons ///
+    // Read
+    const readButton = document.createElement("button");
+    readButton.classList.add("btn-read");
+    readButton.textContent = `${item.read === "Yes" ? "No" : "Yes"}`;
+    // Edit
     const editBtn = document.createElement("button");
     editBtn.textContent = "EDIT";
     editBtn.classList.add("btn-edit");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "DEL";
-    deleteBtn.classList.add("btn-delete");
+    // Delete
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "DEL";
+    deleteButton.classList.add("btn-delete");
 
-    //////
+    // Table Data element
     const tdBtn = document.createElement("td");
-    tdBtn.classList.add("tdButtons");
+    // Add Buttons
+    tdBtn.classList.add("table-buttons");
+    tdBtn.appendChild(readButton);
+    tr.appendChild(tdBtn);
+    table.appendChild(tr);
+
+    /////
     tdBtn.appendChild(editBtn);
     tr.appendChild(tdBtn);
     table.appendChild(tr);
 
     /////
-    tdBtn.appendChild(deleteBtn);
+    tdBtn.appendChild(deleteButton);
     tr.appendChild(tdBtn);
     table.appendChild(tr);
 
-    // Dynamic buttons
-    deleteBtn.addEventListener("click", e => {
-      const row = e.currentTarget;
-      const lostrow = e.currentTarget.parentElement.parentElement;
+    // Book Status Change
+    const statusSelect = document.querySelector(".read-status");
+    // Buttons
+    const btnRead = document.querySelector(".btn-read");
+    const btnDelete = document.querySelector(".btn-delete");
+    //
+    deleteButton.addEventListener("click", e => {
+      let row = e.currentTarget.parentElement.parentElement;
+      row.remove();
+      console.log(row);
 
-      if (row.classList.contains("btn-delete")) {
-        e.currentTarget.parentElement.parentElement.remove();
-      }
+      let theLast = getItemsFromStorage();
+      console.log("Before: ", theLast);
 
-      // Delete from localStorage
-      let items = getItemsFromLocalStorage();
-      items.filter((item, index) => {
-        let newIndex = index + 1;
-        if (newIndex == lostrow.id) {
-          console.log(`${newIndex} matches ${lostrow.id}`);
-          items.splice(newIndex, 1);
-        } else if (index == 0) {
-          items.splice(index, 1);
+      theLast.forEach((item, index) => {
+        if (tr.id == index) {
+          theLast.splice(index, 1);
         }
       });
-      localStorage.setItem("myLibrary", JSON.stringify(items));
-      console.log(items);
+
+      console.log("After: ", theLast);
+      localStorage.setItem("sepLibrary", JSON.stringify(theLast));
     });
     //
     editBtn.addEventListener("click", e => {
       alert("Edit button disabled!");
       console.log("Edit pressed!");
     });
+    //
+    readButton.addEventListener("click", e => {
+      // // Read/Completed Display
+      const yes_no = e.currentTarget.parentElement.previousElementSibling;
+
+      // Check and change read/completed value
+      if (yes_no.innerHTML === "No") {
+        yes_no.innerHTML = "Yes";
+        readButton.innerHTML = "No";
+      } else {
+        yes_no.innerHTML = "No";
+        readButton.innerHTML = "Yes";
+      }
+      // Get localStorage
+      let items = getItemsFromStorage();
+      console.log(items);
+      // Change book read/completed
+      items = items.map(book => {
+        if (book.id == item.id) {
+          book.read = yes_no.textContent;
+        }
+        return book;
+      });
+      // Edit local storage to show changes
+      localStorage.setItem("sepLibrary", JSON.stringify(items));
+      location.reload();
+    });
   });
 }
-function addToLocalStorage() {
-  let newBook = new Book(
-    bookTitle.value,
-    bookAuthor.value,
-    bookPages.value,
-    bookCompleted.value
-  );
 
-  // Check to see if items exist in storage
-  let myLibrary = localStorage.getItem("myLibrary")
-    ? JSON.parse(localStorage.getItem("myLibrary"))
+function addItemsInLocalStorage(nowBook) {
+  let sepLibrary = localStorage.getItem("sepLibrary")
+    ? JSON.parse(localStorage.getItem("sepLibrary"))
     : [];
-
-  // Add new book to library
-  myLibrary.push(newBook);
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-  return myLibrary;
+  sepLibrary.push(nowBook);
+  localStorage.setItem("sepLibrary", JSON.stringify(sepLibrary));
 }
-function getItemsFromLocalStorage() {
-  return localStorage.getItem("myLibrary")
-    ? JSON.parse(localStorage.getItem("myLibrary"))
+function getItemsFromStorage() {
+  return localStorage.getItem("sepLibrary")
+    ? JSON.parse(localStorage.getItem("sepLibrary"))
     : [];
 }
-function deleteItemFromLocalStorage(id) {
-  let items = getLocalStorage();
-  items = items.filter(item => {
-    if (item.id !== id) {
-      return item;
-    }
-  });
-  localStorage.setItem("myLibrary", JSON.stringify(items));
+
+// Update Storage
+function updateLocalStorage(newArray) {
+  let sepLibrary = localStorage.getItem("sepLibrary")
+    ? JSON.parse(localStorage.getItem("sepLibrary"))
+    : [];
+  sepLibrary.push(nowBook);
+  localStorage.setItem("sepLibrary", JSON.stringify(sepLibrary));
 }
+function resetAllForm() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.value = "";
+  title.focus();
+}
+
 // Clear all fields function
-const clearItems = () => {
-  bookTitle.value = "";
-  bookAuthor.value = "";
-  bookPages.value = "";
-  bookCompleted.value = "";
-};
+// const clearItems = () => {
+//   title.value = "";
+//   author.value = "";
+//   pages.value = "";
+//   pages.value = "";
+// };
 
 // ------------------------ //
 // Button Events //
-btnAdd.addEventListener("click", () => {
+addBtn.addEventListener("click", () => {
   modal.classList.add("modal-show");
-  clearItems();
+  // clearItems();
+  console.log("Add Sup!");
 });
-
-// Reload to add a new entry
-submitBtn.addEventListener("click", () => {});
 
 // Close Modal
 closeModal.addEventListener("click", () => {
@@ -210,5 +244,21 @@ closeModal.addEventListener("click", () => {
 //   alert("Clear All button disbled!");
 // });
 
-// Display Library on page load
-window.addEventListener("DOMContentLoaded", displayOnPage(myLibrary));
+// Events
+submitBtn.addEventListener("click", () => {
+  if (
+    title.value === "" ||
+    author.value === "" ||
+    pages.value === "" ||
+    read.value === ""
+  ) {
+    alert("You must fill all fields!");
+    return;
+  }
+
+  location.reload();
+  title.focus();
+});
+
+// Load library display
+window.addEventListener("DOMContentLoaded", createBookData(sepLibrary));
